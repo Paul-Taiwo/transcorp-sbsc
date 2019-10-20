a1d44fba25d1ccaa3ff5387c50f926b2e7181d78
BeardKoda <akinsuyi.joshua842gmail.com>
2019-10-21 00:38:55 +0100

    add icons

--- src/app/components/blog-card/blog-card.component.ts
+++ src/app/components/blog-card/blog-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-blog-card',
@@ -6,6 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./blog-card.component.css']
 })
 export class BlogCardComponent implements OnInit {
+
+  @Input() blogCardImgSrc: string;
+  @Input() blogCardTitle: string;
+  @Input() blogCardDate: string;
 
   constructor() { }
 
--- src/app/components/discover/discover.component.ts
+++ src/app/components/discover/discover.component.ts
@@ -12,6 +12,26 @@ export class DiscoverComponent implements OnInit {
     { roomType: 'Ultra Suprior Room', imgSrc:"../../../assets/images/5D3D5ABE-FB24-45D0-800E-C354968BF1AD.png"},
     { roomType: 'Classic Room', imgSrc:"../../../assets/images/053CA9AC-3595-4FE8-B43E-E823476A5B3A.png"},
   ]
+
+  public blogPosts: Object[] = [
+    { 
+      blogCardTitle: 'Five reasons to  stay at the beautiful Transcorp Hotel',
+      blogCardImgSrc: "../../../assets/images/053CA9AC-3595-4FE8-B43E-E823476A5B3A.png", 
+      blogCardDate: "April 10, 2019" 
+    },
+    { 
+      blogCardTitle: 'Transcorp Hilton Abuja Wins Five Awards', 
+      blogCardImgSrc: "../../../assets/images/0FAC46BE-9D95-4DCE-AADA-3AF5FDEF4E31.png", 
+      blogCardDate: "June 5, 2019"
+     },
+    { 
+      blogCardTitle: 'Ultra Suprior Room', 
+      blogCardImgSrc: "../../../assets/images/5D3D5ABE-FB24-45D0-800E-C354968BF1AD.png", 
+      blogCardDate: "May 23, 2019" 
+    },
+  ]
+
+
   constructor() { }
 
   ngOnInit() {
